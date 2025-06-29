e4d750f51a229e253c70bf9bb3158914121728a2
Ankit Sharma <ankitsh474@gmail.com>
2025-06-29 22:27:43 +0530

    feat: update CORS configuration to allow requests from any origin

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
